caede0bed70b039911b658a6bcc0522edd9446b6
Atru8729 <a.truskauskas1@gmail.com>
2021-05-06 20:38:19 +0300

    objects

--- js/10-switch.js
+++ js/10-switch.js
@@ -34,6 +34,7 @@ switch (day) {
     case 5:
         console.log('Darbo diena');
         break;
+
     case 6:
     case 7:
         console.log('Savaitgalis');
@@ -42,4 +43,62 @@ switch (day) {
     default:
         console.log('Neatpazinta savaites diena');
         break;
+}
+
+console.log('......................');
+
+/*
+Arbatos paruosimo procesas:
+0) paimti puodeli
+1) isideti arbatos
+2) isideti saldikli
+3) uzpilti vandens
+4) ismaisyti
+5) gerti
+*/
+
+const processStep = 3;
+
+switch (processStep) {
+    case 0:
+        console.log('paimiti puodeli');
+        
+    case 1:
+        console.log('isideti arbatos');
+        
+    case 2:
+        console.log('uzpilti vandens');
+        
+    case 3:
+        console.log('ismaisyti');
+        
+    case 5:
+        console.log('gerti');
+        break;
+
+    default:
+        console.log('Neatpazintas procesas');
+        break;
+}
+
+console.log('.....................');
+
+const stotele = 'Geniu';
+
+switch (stotele) {
+    case 'Geniu':
+        console.log('Geniu');
+    case 'Tremtiniu':
+            console.log('Tremtiniu');
+    case 'Rudens':
+        console.log('Rudens');
+    case 'K. Mindaugo':
+        console.log('K. Mindaugo');
+    case 'Z. Tiltas':
+        console.log('Z. Tiltas');
+        break;
+
+    default:
+        console.log('Neatpazinta stotele');
+        break;    
 }
