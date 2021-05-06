4b34e32b9f5573db19ce1cfae272e8d172ff822e
Atru8729 <a.truskauskas1@gmail.com>
2021-05-06 18:59:06 +0300

    Nestintas ternary

--- js/09-ternary.js
+++ js/09-ternary.js
@@ -10,7 +10,9 @@ const arLyja = false;
 const sketis = arLyja ? 'pasiimk' : 'nereikia';    // pasiiimk / nereikia
 console.log(sketis);
 
-const s1 = 4 < 2 ? 'daugiau' : 'ne daugiau';
+const s1 = 4 < 2 
+                ? 'daugiau' 
+                : 'ne daugiau';
 console.log(s1);
 
 let s2 = '';
@@ -20,3 +22,16 @@ if (4 < 2) {
     s2 = 'ne daugiau';
 }
 console.log(s2);
+
+console.log('............................');
+
+/*
+Nestintas ternary
+*/
+
+const color ='red';
+const light = true;
+
+const x1 = color === 'blue' ? light ? 'Sviesiai melyna' : 'Tamsiai melyna' : color === 'red' ? 'Raudona' : 'Nezinoma spalva';
+
+console.log(x1);
