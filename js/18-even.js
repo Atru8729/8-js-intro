6e5f8d180539306db2b0ee3b2512dae9e204558c
Atru8729 <a.truskauskas1@gmail.com>
2021-05-07 20:15:00 +0300

    multi, day, even, love

---
+++ js/18-even.js
@@ -0,0 +1,11 @@
+function even(n) {
+    return n % 2 === 0;
+    }
+
+
+    // return true;  jeigu lyginis skaicius
+    // return false; jeigu nelyginis skaicius
+
+
+console.log(even(7), '->', false);
+console.log(even(70), '->', true);
