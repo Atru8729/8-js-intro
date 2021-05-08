2d2cbee89fa264d4a942b48293b0663e35b4fe05
Atru8729 <a.truskauskas1@gmail.com>
2021-05-08 11:00:49 +0300

    GetPlanetName, check

---
+++ codewars/check.js
@@ -0,0 +1,9 @@
+function check(a, x) {
+    for (let i = 0; i < a.length; i++)
+  }
+
+console.log(check([66, 101], 66), '->' true);
+
+//console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+//Test.assertEquals(check(['t', 'e', 's', 't'], 'e'), true);
+//Test.assertEquals(check(['what', 'a', 'great', 'kata'], 'kat'), false);
