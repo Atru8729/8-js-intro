0dc003e2609a643e2e2c828239f71e17151a467e
Atru8729 <a.truskauskas1@gmail.com>
2021-05-08 12:23:40 +0300

    array Madness

---
+++ codewars/correctPolishLetters.js
@@ -0,0 +1,29 @@
+/*
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+*/
+
+function correctPolishLetters (string) {
+    let convertedString = '';
+
+    // einame per raides
+        // paimu raide
+        // jeigu raide yra ne lenkiska
+            // tiesiog pridedame prie convertedString
+        // jeigu tai lenkiska
+            // tiesiog pridedame prie convertedString atitinkama lotiniska tos raides versija
+  
+    return convertedString;
+  }
+
+
+  console.log(correctPolishLetters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+  console.log(correctPolishLetters("Lech Wałęsa"),"Lech Walesa");
+  console.log(correctPolishLetters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie");
